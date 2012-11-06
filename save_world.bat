822ccc762d2cc0f64e27602cab6baa1f00f263e7
Ron Gross <ron.gross@gmail.com>
2012-11-06 19:18:55 +0200

    Oops

--- save_world.bat
+++ save_world.bat
@@ -1,14 +1,1 @@
-REM This is just a silly demo script
-REM Nothing to see here
-
-@echo "This script will save the world"
-
-@echo "How, you ask?"
-@echo "Well, line by line"
-
-@echo "Yet another boring, silly line"
-
-
-
-
-@echo "This will be the last line of this script"
+sdgadsf

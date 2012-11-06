c2b727dafba9119ce4deb0076a14190c732d718f
Ron Gross <ron.gross@gmail.com>
2012-11-06 19:22:51 +0200

    Revert "Oops"
    This reverts commit 822ccc762d2cc0f64e27602cab6baa1f00f263e7.

--- save_world.bat
+++ save_world.bat
@@ -1,1 +1,14 @@
-sdgadsf
+REM This is just a silly demo script
+REM Nothing to see here
+
+@echo "This script will save the world"
+
+@echo "How, you ask?"
+@echo "Well, line by line"
+
+@echo "Yet another boring, silly line"
+
+
+
+
+@echo "This will be the last line of this script"
